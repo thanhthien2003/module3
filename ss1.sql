50f25e0885ecc867dbe631d715bb895c38723418
unknown <thanhthien2003>
2023-05-15 14:45:42 +0700

    Message

--- ss1.sql
+++ ss1.sql
@@ -1,15 +1,15 @@
-create database ss1;	
-create table ss1(
+create database bai_tap1;	
+use bai_tap1;
+create table teacher(
 id int primary key auto_increment,
 `name` varchar(45) null,
 country varchar(45) null,
 age int null
 );
-
-alter table ss1 rename to Teacher;
 create table Class(
 id int primary key auto_increment,
 name varchar(45) null
 );
 select * from Class;
 select * from Teacher;
+
